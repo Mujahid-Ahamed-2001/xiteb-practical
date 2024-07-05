0a14fb404ec18a13db409a2624514b6e495dbc3a
“Mujahid-Ahamed-2001” <mujahidmjhd2001@gmail.com>
2024-07-05 20:50:24 +0530

    body

--- resources/views/user/body.blade.php
+++ resources/views/user/body.blade.php
@@ -13,7 +13,7 @@
               <div class="row">
                 <div class="col-9">
                   <div class="d-flex align-items-center align-self-start">
-                    <h3 class="mb-0">$12.34</h3>
+                    <h3 class="mb-0">Rs12.34</h3>
                     <p class="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
                   </div>
                 </div>
